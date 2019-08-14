384cdcb891f997f6c99faebcda2056caf9f952e1
Iuwqyir <toomas.oosalu@gmail.com>
2019-08-14 18:07:39 +0100

    MOD refactor so UI is updated without refreshing

--- src/App.js
+++ src/App.js
@@ -1,70 +1,31 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config';
 import TodoList from './TodoList';
 
 class App extends Component {
   componentDidMount() {
-    this.loadBlockchainData();
+    this.loadUserAddress();
   }
 
-  async loadBlockchainData() {
+  async loadUserAddress() {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
-    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
-    this.setState({ todoList });
-    const todoItemCount = await todoList.methods.todoItemCount().call();
-    const newItemIndex = await todoList.methods.newItemIndex().call();
-    this.setState({ todoItemCount });
-    for (var i = 0; i < newItemIndex; i++) {
-      const todoItem = await todoList.methods.todoItems(i).call();
-      if (todoItem.title.length > 0 && todoItem.content.length > 0) {
-        this.setState({
-          todoItems: [todoItem, ...this.state.todoItems]
-        });
-      }
-    }
-    this.setState({ loading: false });
+    this.setLoading(false);
+  }
+
+  setLoading(loading) {
+    this.setState({loading});
   }
 
   constructor(props) {
     super(props);
     this.state = {
       account: '',
-      todoItemCount: 0,
-      todoItems: [],
       loading: true
     };
-    this.createTodoItem = this.createTodoItem.bind(this);
-    this.toggleCompleted = this.toggleCompleted.bind(this);
-    this.deleteTodoItem = this.deleteTodoItem.bind(this);
-  }
-
-  createTodoItem(title, content) {
-    this.setState({ loading: true });
-    this.state.todoList.methods.createTodoItem(title, content).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false });
-    });
-  }
-
-  toggleCompleted(id) {
-    this.setState({ loading: true });
-    this.state.todoList.methods.toggleCompleted(id).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false });
-    });
-  }
-
-  deleteTodoItem(id) {
-    console.log(id);
-    this.setState({ loading: true });
-    this.state.todoList.methods.deleteTodoItem(id).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false });
-    });
+    this.setLoading = this.setLoading.bind(this);
   }
 
   render(){
@@ -83,11 +44,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex justify-content-center">
               { this.state.loading 
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div> 
-                : <TodoList 
-                  todoItems={this.state.todoItems} 
-                  createTodoItem={this.createTodoItem}
-                  toggleCompleted={this.toggleCompleted} 
-                  deleteTodoItem={this.deleteTodoItem} /> }
+                : <TodoList setLoading={this.setLoading} account={this.state.account} /> }
             </main>
           </div>
         </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,44 +1,85 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import Web3 from 'web3';
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config';
 
 class TodoList extends Component {
   componentDidMount() {
-    this.createElements();
+    this.loadContractData();
   }
 
   constructor(props) {
     super(props);
     this.state = {
       completedItems: [],
-      uncompletedItems: []
+      uncompletedItems: [],
+      todoItemCount: 0
     };
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+    this.deleteTodoItem = this.deleteTodoItem.bind(this);
   }
 
-  createElements() {
+  async loadContractData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+    this.setState({ todoList });
+    const todoItemCount = await todoList.methods.todoItemCount().call();
+    const newItemIndex = await todoList.methods.newItemIndex().call();
+    this.setState({ todoItemCount });
+
     let completedItems = [];
     let uncompletedItems = [];
-    this.props.todoItems.map((todoItem, key) => {
+    for (var i = 0; i < newItemIndex; i++) {
+      const todoItem = await todoList.methods.todoItems(i).call();
+      if (todoItem.title.length > 0 && todoItem.content.length > 0) {
+        const element = <TodoItem key={todoItem.id} id={todoItem.id} todoItem={todoItem} toggleCompleted={this.toggleCompleted} deleteTodoItem={this.deleteTodoItem} />;
         if (todoItem.isCompleted) {
           completedItems.push(
-            <TodoItem
-              key={key} 
-              id={key}
-              todoItem={todoItem}
-              toggleCompleted={this.props.toggleCompleted}
-              deleteTodoItem={this.props.deleteTodoItem} />);
+          <TodoItem
+            key={todoItem.id} 
+            id={todoItem.id}
+            todoItem={todoItem}
+            toggleCompleted={this.toggleCompleted}
+            deleteTodoItem={this.deleteTodoItem} />);
         } else {
           uncompletedItems.push(
             <TodoItem
-              key={key} 
-              id={key}
-              todoItem={todoItem}
-              toggleCompleted={this.props.toggleCompleted}
-              deleteTodoItem={this.props.deleteTodoItem} />);
+            key={todoItem.id} 
+            id={todoItem.id}
+            todoItem={todoItem}
+            toggleCompleted={this.toggleCompleted}
+            deleteTodoItem={this.deleteTodoItem} />);
         }
-    });
-
+      }
+    }
+    console.log(uncompletedItems);
     this.setState({uncompletedItems});
     this.setState({completedItems});
+    this.props.setLoading(false);
+  }
+
+  createTodoItem(title, content) {
+    this.props.setLoading(true);
+    this.state.todoList.methods.createTodoItem(title, content).send({ from: this.props.account })
+    .once('receipt', (receipt) => {
+      this.props.setLoading(false);
+    });
+  }
+
+  toggleCompleted(id) {
+    this.props.setLoading(true);
+    this.state.todoList.methods.toggleCompleted(id).send({ from: this.props.account })
+    .once('receipt', (receipt) => {
+      this.props.setLoading(false);
+    });
+  }
+
+  deleteTodoItem(id) {
+    this.props.setLoading(true);
+    this.state.todoList.methods.deleteTodoItem(id).send({ from: this.props.account })
+    .once('receipt', (receipt) => {
+      this.props.setLoading(false);
+    });
   }
 
   render(){
@@ -46,7 +87,7 @@ class TodoList extends Component {
       <div id="content">
         <form onSubmit={(event) => {
           event.preventDefault();
-          this.props.createTodoItem(this.todoItemTitle.value, this.todoItemContent.value);
+          this.createTodoItem(this.todoItemTitle.value, this.todoItemContent.value);
         }}>
           <div className="form-group">
             <input 
